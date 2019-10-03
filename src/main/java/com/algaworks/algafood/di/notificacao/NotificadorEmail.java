108358c75d84e61a46d60749f449ab949dc042a6
Jose Marcio Dias dos Santos <jose.dossantos@totvs.com.br>
2019-10-03 15:12:47 -0300

    Injeção e anotação @AutoWired

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,29 +1,16 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Component
 public class NotificadorEmail implements Notificador {
-	
-	private boolean caixaAlta;
-	private String hostServidorSMTP;
-	
-	public NotificadorEmail(String hostServidorSMTP) {
-		this.hostServidorSMTP = hostServidorSMTP;
-		System.out.println("NotificadorEmail");
-	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if (this.caixaAlta) {
-			mensagem =mensagem.toUpperCase();
-		}
 		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), this.hostServidorSMTP, mensagem);
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
-
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
-	
 }
